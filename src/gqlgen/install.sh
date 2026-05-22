b164ee8adacd61e47c73d84ac119dd8b0c9f1728
Ben Cessa <ben@pixative.com>
2026-05-22 09:13:06 -0600

    Buf v1.69.0

--- src/gqlgen/install.sh
+++ src/gqlgen/install.sh
@@ -34,7 +34,7 @@ fi
 
 echo -e "Activating feature: 'gqlgen'"
 echo -e "Installing tools"
-GQLGEN="0.17.81"
+GQLGEN="0.17.89"
 GO_TOOLS="\
   github.com/99designs/gqlgen@v${GQLGEN}"
 export PATH=/go/bin:${PATH}
